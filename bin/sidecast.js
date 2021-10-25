95b8cc70a202499248a4cbff1d29352e411aa21e
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2021-10-24 22:44:08 -0700

    Fix installs
    Fix installs, add purge command, update help menu, add aliases, default to main branch selected instead of master, add more instructions to install command

--- bin/sidecast.js
+++ bin/sidecast.js
@@ -3,6 +3,13 @@ const chalk = require('chalk');
 const Stump = require('stump.js');
 const os = require('os');
 const stump = new Stump(['Debug']);
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+  
+const fs = require('fs').promises;
 const { exec } = require('child_process');
 const fetch = require('node-fetch');
 const download = require('download-git-repo');
@@ -34,7 +41,8 @@ const commands = {
         console.log('\n' + chalk.bgRed.bold.white(ascii));
         console.log(chalk.bold('\nSidecast Commands:' + `
 ${chalk.blue('help')} - Show the help menu
-${chalk.blue('sideload <github repo>')} - Sideload an extension (GitHub Repo is user/repo#branch, defaulting to master)
+${chalk.blue('sideload <github repo> [-v, --verbose]')} - Sideload an extension (GitHub Repo is user/repo#branch) ${chalk.blueBright('[install, i]')}
+${chalk.blue('remove <extension name> [-v, --verbose]')} - Remove the files for an extension ${chalk.blueBright('[purge]')}
 
 More commands will be added soon!`));
     },
@@ -50,6 +58,7 @@ More commands will be added soon!`));
                 return stump.error('Must be a URL or GitHub repository (user/repo#branch)');
             }
         }
+        if (!ghRepo.startsWith('direct:') && !ghRepo.includes('#')) ghRepo += '#' + json.default_branch;
         let fragments;
         if (ghRepo.startsWith('direct:')) {
             fragments = fragments.filter(frag => frag !== '');
@@ -69,24 +78,55 @@ More commands will be added soon!`));
             stump.info('Installing dependencies...');
             var yourscript = exec('npm install', { cwd: destination },
             (error, stdout, stderr) => {
-                stump.info('Building extension...');
-                exec('./node_modules/@raycast/api/bin/ray build -e dist', { cwd: destination}, (error, stdout, stderr) => {
-                if (stderr) {
-                    if (args.includes('-v') || args.includes('--verbose'))
-                    return stump.error('There was an error while trying to build this extension. For more details, use the ' + chalk.blue('--verbose') + '(' + chalk.blue('-v') + ') flag to use verbose mode.')
-                    else return stump.error('There was an error while trying to build this extension. Errors: ' + stdout);
-                }
-                stump.success('Extension downloaded! Here\'s what\'s next:\n' + `
-    ${chalk.bold('1.')} Open Raycast and run the ${chalk.blue('Import Extension ' + chalk.dim('Developer'))} command.
-    ${chalk.bold('2.')} Navigate to ${chalk.blue('~/_sidecast')} and select ${chalk.blue(((fragments.length >= 2 ? fragments[fragments.length - 2] + "/" : '') + fragments[fragments.length - 1]).toLowerCase())}
-    ${chalk.bold('3.')} Start using your extension!`);
-                resolve();
+                stump.success('Extension downloaded! Before the extension can be built, you have to load it first.\n' + `
+${chalk.bold('1.')} Open Raycast and run the ${chalk.blue('Import Extension ' + chalk.dim('Developer'))} command.
+${chalk.bold('2.')} Navigate to ${chalk.blue('~/_sidecast')} and select ${chalk.blue(((fragments.length >= 2 ? fragments[fragments.length - 2] + "/" : '') + fragments[fragments.length - 1]).toLowerCase())}`);
+                readline.question(`${chalk.bold('3.')} Press enter to continue`, () => {
+                    console.log('');
+                    stump.info('Building extension...');
+                    exec('./node_modules/@raycast/api/bin/ray build -e dist', { cwd: destination }, (error, stdout, stderr) => {
+                        console.log(stdout, stderr);
+                        if (stderr) {
+                            if (args.includes('-v') || args.includes('--verbose'))
+                            return stump.error('There was an error while trying to build this extension. For more details, use the ' + chalk.blue('--verbose') + '(' + chalk.blue('-v') + ') flag to use verbose mode.')
+                            else return stump.error('There was an error while trying to build this extension. Errors: ' + stdout);
+                        }
+                        stump.success('Extension built! You can start using it.');
+                        resolve();
+                    });
                 });
             });
         });
     });
           
-    }
+    },
+    'remove': async ([extension, ...args]) => {
+        let correctPath;
+        try {
+            let path = os.homedir() + '/_sidecast/' + extension.toLowerCase();
+            await fs.access(path);
+            correctPath = path;
+        } catch (err) {
+            try {
+                let path = os.homedir() + '/_sidecast/' + extension.toLowerCase().split('/').join(':');
+                await fs.access(path);
+                correctPath = path;
+            } catch (err) {
+                return stump.error("The extension folder cannot be located. Please try manually deleting it instead.");
+            }
+        }
+        try {
+            fs.rm(correctPath, { recursive: true, force: true });
+            stump.success('The extension ' + chalk.blue(extension) + ' was removed. You will also have to uninstall it from within Raycast by finding the extension, clicking more actions, and selecting uninstall.');
+        } catch (err) {
+            if (args.includes('-v') || args.includes('--verbose'))
+            return stump.error('There was an error while trying to remove this extension. For more details, use the ' + chalk.blue('--verbose') + '(' + chalk.blue('-v') + ') flag to use verbose mode.')
+            else return stump.error('There was an error while trying to remove this extension. Errors: ' + err);
+        }
+    },
+    install: (...args) => commands.sideload(...args),
+    purge: (...args) => commands.remove(...args),
+    i: (...args) => commands.sideload(...args)
 };
 
 const defaultCommand = (command) => {
@@ -99,6 +139,7 @@ if (commands[command]) {
         let output = commands[command](params);
         if (output instanceof Promise) await output;
         console.log('');
+        process.exit();
     })();
 }
 else defaultCommand(command);
